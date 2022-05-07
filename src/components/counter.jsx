aca94602a062d6670b8d798594ac6a5ca57191a9
Khalid Ameen <kamenphd@gmail.com>
2022-05-07 17:33:00 -0400

    Added Counters class.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 export default class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     imageUrl: "https://picsum.photos/200",
-    tags: [],
+    tags: ["tag1"],
   };
   styles = {
     fontSize: 10,
@@ -23,8 +23,8 @@ export default class Counter extends Component {
           Increament
         </button>
         <div>
-          <div>{this.state.tags.length === 0 && "Please create a new tag"}</div>
-          {this.renderTags()}
+          {/* <div>{this.state.tags.length === 0 && "Please create a new tag"}</div>
+          {this.renderTags()} */}
         </div>
       </React.Fragment>
     );
@@ -32,11 +32,11 @@ export default class Counter extends Component {
 
   handleIncreament = (id) => {
     console.log(id);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
-    if (this.state.count === 0) return "There are no Tags!";
+    if (this.state.value === 0) return "There are no Tags!";
     else
       return (
         <ul>
@@ -50,12 +50,12 @@ export default class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactRoot from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
 // const container = document.getElementById("root");
 // const root = CreateRoot(container);
 // root.render(<Counter/>);
 
-ReactRoot.render(<Counter/>, document.getElementById("root"));
+ReactRoot.render(<Counters/>, document.getElementById("root"));
